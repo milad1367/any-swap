a1c27b0d07d3274c9623f7b4ba31c832bc55654d
Milad Asghari Amlashi <miladasghari2016@gmail.com>
2022-06-26 11:07:20 +0300

    Boomgit add first connector

--- src/components/swapper/Swapper.tsx
+++ src/components/swapper/Swapper.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from "../../config";
 
 import "./Swapper.scss";
 
@@ -7,6 +9,28 @@ const targets = ["", "usdc(erc20)"];
 
 function Swapper() {
   const [state, setState] = useState({ source: "", target: "" });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState({} as any);
+  useEffect(() => {
+    async function loadBlockchainData() {
+      const web3 = new Web3("http://localhost:7545");
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+      const _contract = new web3.eth.Contract(
+        TODO_LIST_ABI as any,
+        TODO_LIST_ADDRESS
+      );
+      setContract(contract);
+
+      _contract.methods
+        .taskCount()
+        .call()
+        .then((res: any) => console.log(res));
+    }
+    loadBlockchainData();
+  }, [contract]);
+
   const handleChange = (e: any) => {
     const { name, value } = e.target;
     setState((prev) => ({ ...prev, [name]: value }));
---
+++ src/config.ts
@@ -0,0 +1,123 @@
+export const TODO_LIST_ADDRESS = "0x85381a04d95cdac1dd7cc3abb204d4abbf9ecccd";
+
+export const TODO_LIST_ABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "completed",
+        type: "bool",
+      },
+    ],
+    name: "TaskCompleted",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "content",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "completed",
+        type: "bool",
+      },
+    ],
+    name: "TaskCreated",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "taskCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "tasks",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "content",
+        type: "string",
+      },
+      {
+        internalType: "bool",
+        name: "completed",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_content",
+        type: "string",
+      },
+    ],
+    name: "createTask",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "toggleCompleted",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
